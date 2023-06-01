d9247278c40c0c47a55a9f499abc8162c3e96c91
alfalakk <kinanalfalakh@gmail.com>
2023-06-01 04:19:07 +0200

    Added .gitignore and src file

---
+++ A&G/src/Main.hs
@@ -0,0 +1,11 @@
+-- Main functin will be here 
+-- Separate IO from project
+import SubsetConstruction
+
+
+main :: IO ()
+main = do
+  -- read NFA
+  -- apply subsetConstruction
+  -- print DFA
+     return ()
---
+++ A&G/src/SubsetConstruction.hs
@@ -0,0 +1,16 @@
+module SubsetConstruction where
+import Data.Set (Set)
+import qualified Data.Set as Set
+
+-- type State = Int
+-- type Transition = (State, Maybe Char, State)
+
+-- type NFA = (Set State, Set Transition, State, Set State)
+-- type DFA = (Set State, Set Transition, State, Set State)
+
+
+-- epsilonClosure :: NFA -> State -> Set State
+
+-- move :: NFA -> Set State -> Char -> Set State
+
+-- subsetConstruction :: NFA -> DFA
